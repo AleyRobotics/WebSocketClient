b040d3c39eedaac391694a8fad0f4e3369871aa3
Yuri Aleynikov <Ysoft@mail.ru>
2019-03-22 12:15:36 +0300

    version update
    version update

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "WebSocketClient",
     dependencies: [
         .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Aleyrobotics/HTTPClient.git", majorVersion: 0, minor: 14),
     ]
 )
